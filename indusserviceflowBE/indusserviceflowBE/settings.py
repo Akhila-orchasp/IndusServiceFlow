93199ef75b0d7e6ddf4cbf4ce63e0f58c136672d
akhila.p <akhila.p@orchasp.com>
2026-09-23 11:24:05 +0530

    Modified the settings.py

--- indusserviceflowBE/indusserviceflowBE/settings.py
+++ indusserviceflowBE/indusserviceflowBE/settings.py
@@ -193,6 +193,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://10.168.130.27:5173",
+    "https://indus-service-flow.vercel.app",3
 ]
 
 CORS_ALLOW_CREDENTIALS = True
